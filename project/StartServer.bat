ebb3e3f5aada7e0d96fbd66add7e6c382fdfe14d
ylmbtm@163.com <ylmbtm@163.com>
2014-06-23 21:45:16 +0800

    upload change

--- project/StartServer.bat
+++ project/StartServer.bat
@@ -13,12 +13,21 @@ echo.             3.关闭全部服务器
 echo.
 echo.             4.退出命令窗口
 echo.
+echo.
+set DebugDir=%cd%\Server\Debug\
+set ReleaseDir=%cd%\Server\Release\
+echo.
+echo Debug = %DebugDir%
+echo Release= %ReleaseDir%
+echo.
+
+
 :cho
 set choice=
 set /p choice=          请选择:
 IF NOT "%choice%"=="" SET choice=%choice:~0,1%
-if /i "%choice%"=="1" start /D Server\Debug\ /MIN ./Server/Debug/CenterServer.exe & start /D .\Server\Debug\ /MIN ./Server/Debug/LoginServer.exe & start /D .\Server\Debug\ /MIN ./Server/Debug/ProxyServer.exe & start /D .\Server\Debug\ /MIN ./Server/Debug/DBServer.exe & start /D .\Server\Debug\ /MIN ./Server/Debug/GameServer.exe
-if /i "%choice%"=="2" start /D Server\Release\ /MIN ./Server/Release/CenterServer.exe & start /D .\Server\Release\ /MIN ./Server/Release/LoginServer.exe & start /D .\Server\Release\ /MIN ./Server/Release/ProxyServer.exe & start /D .\Server\Release\ /MIN ./Server/Release/DBServer.exe & start /D .\Server\Release\ /MIN ./Server/Release/GameServer.exe
+if /i "%choice%"=="1" start /D %DebugDir% /MIN %DebugDir%CenterServer.exe & start /D %DebugDir% /MIN %DebugDir%LoginServer.exe & start /D %DebugDir% /MIN %DebugDir%ProxyServer.exe & start /D %DebugDir% /MIN %DebugDir%DBServer.exe & start /D %DebugDir% /MIN %DebugDir%GameServer.exe
+if /i "%choice%"=="2" start /D %DebugDir% /MIN %DebugDir%CenterServer.exe & start /D %DebugDir% /MIN %DebugDir%LoginServer.exe & start /D %DebugDir% /MIN %DebugDir%ProxyServer.exe & start /D %DebugDir% /MIN %DebugDir%DBServer.exe & start /D %DebugDir% /MIN %DebugDir%GameServer.exe
 if /i "%choice%"=="3" taskkill /im CenterServer.exe & taskkill /im LoginServer.exe & taskkill /im ProxyServer.exe & taskkill /im DBServer.exe & taskkill /im GameServer.exe
 if /i "%choice%"=="4" exit
 goto cho
